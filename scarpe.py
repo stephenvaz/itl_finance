d4ce60152f90f489ec321263b9c5a802e5baf0c7
Stephen Vaz <stephenvaz2003@gmail.com>
2023-11-07 17:35:13 +0530

    few updates

--- scarpe.py
+++ scarpe.py
@@ -3,14 +3,11 @@ import pandas as pd
 
 session = HTMLSession()
 
-#use session to get the page
 r = session.get('https://economictimes.indiatimes.com/reliance-industries-ltd/stocksupdate/companyid-13215.cms')
 
-#render the html, sleep=1 to give it a second to finish before moving on. scrolldown= how many times to page down on the browser, to get more results. 5 was a good number here
 r.html.render(sleep=1, scrolldown=5)
 
 articleContainer = r.html.find('div.eachStory')
-# print(len(articleContainer))
 
 articleList = []
 
@@ -29,9 +26,6 @@ for article in articleContainer:
     except:
         pass
 
-# print(articleList[0])
-
-# create a csv file with title, date, info
 df = pd.DataFrame(articleList)
 df.to_csv('article.csv', index=False)
 print(df.head())
